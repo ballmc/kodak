eaef3a5334b5a6becc1b089f772e50bfdb42cf74
ballmc <goreypvp@gmail.com>
2023-12-29 23:26:33 -0800

    shit code update

--- src/main/java/me/ballmc/Kodak/mixins/erm.java
+++ src/main/java/me/ballmc/Kodak/mixins/erm.java
@@ -2,25 +2,56 @@ package me.ballmc.Kodak.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.Debug;
+
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ChatComponentText;
 
+import me.ballmc.Kodak.Kodak;
+import me.ballmc.Kodak.Config;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 @Mixin(EntityRenderer.class)
 public abstract class erm {
-    @Redirect(
-        method = "orientCamera",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/renderer/GlStateManager;translate(FFF)V",
-            ordinal = 2
-        )
-    )
-    public void redirectTranslate(float x, float y, float z) {
-      Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("Redirected!"));
-      GlStateManager.translate(0.0F, 0.0F, (float)(-4.0D));
+    Kodak kodak = Kodak.getInstance();
+    Config config = kodak.getConfig();
+
+    // @SuppressWarnings("ConstantConditions")
+    // @Redirect(
+    //     method = "orientCamera",
+    //     at = @At(
+    //         value = "INVOKE",
+    //         target = "Lnet/minecraft/client/renderer/GlStateManager;translate(FFF)V",
+    //         ordinal = 2
+    //     )
+    // )
+
+    @ModifyArg(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;translate(FFF)V", ordinal = 2), index = 2, remap = false)
+    private float modifyThirdArgInGLStateManagerTranslate(float x){
+        if (config.KodakEnabled) {
+            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("kodak on!"));
+            return (float)(config.KodakDistance * -1);
+        } else {
+            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("kodak off!"));
+            return (float)(-x);
+        }
     }
+
+    // public void redirectTranslate(float x, float y, float z) {
+    //   Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("Redirected!"));
+    //   if (config.KodakEnabled) {
+    //     GlStateManager.translate(0.0F, 0.0F, (float)(config.KodakDistance * -1));
+    //   } else {
+    //     GlStateManager.translate(0.0F, 0.0F, (float)(-4.0D));
+    //   }
+    // }
 }
